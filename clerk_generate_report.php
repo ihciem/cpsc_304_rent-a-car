35f747b3d826ebe3ef5a8eac63a31c879c194d5d
darrenjhong <darrenjhong7@live.ca>
2019-11-22 23:27:34 -0800

    Added image to select user

--- clerk_generate_report.php
+++ clerk_generate_report.php
@@ -1,22 +1,12 @@
-<!--Test Oracle file for UBC CPSC304 2018 Winter Term 1
-  Created by Jiemin Zhang
-  Modified by Simona Radu
-  Modified by Jessica Wong (2018-06-22)
-  This file shows the very basics of how to execute PHP commands
-  on Oracle.
-  Specifically, it will drop a table, create a table, insert values
-  update values, and then query for values
-  IF YOU HAVE A TABLE CALLED "demoTable" IT WILL BE DESTROYED
-  The script assumes you already have a server set up
-  All OCI commands are commands to the Oracle libraries
-  To get the file to work, you must place it somewhere where your
-  Apache server can run it, and you must rename it to have a ".php"
-  extension.  You must also change the username and password on the
-  OCILogon below to be your ORACLE username and password -->
-
   <html>
     <head>
-        <title>CPSC 304 PHP/Oracle Demonstration</title>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
     </head>
 
     <body>
@@ -287,7 +277,7 @@
           }
       }
   ?>
-        <h2>Generate reports</h2>
+        <h2>Generate Reports</h2>
         <form method="GET" action="clerk_generate_report.php"> <!--refresh page when submitted-->
             <input type="hidden" id="generateReportRequest" name="generateReportRequest">
             Type: <input type="radio" name="reportType" value="rentals"> Rentals
