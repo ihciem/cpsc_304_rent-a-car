1ceb3c783381be3f2e1971fd897e1d75252e32ed
Mei Chi <meichi.chin98@gmail.com>
2019-11-24 15:51:49 -0800

    Updated to use connectToDB.php file

--- clerk_generate_report.php
+++ clerk_generate_report.php
@@ -13,7 +13,6 @@ To get the file to work, you must place it somewhere where your
 Apache server can run it, and you must rename it to have a ".php"
 extension.  You must also change the username and password on the
 OCILogon below to be your ORACLE username and password -->
-
 <html>
 <head>
   <!-- Required meta tags -->
@@ -26,76 +25,59 @@ OCILogon below to be your ORACLE username and password -->
 </head>
 
 <body>
+
   <?php //this tells the system that it's no longer just parsing html; it's now parsing PHP
+
   include 'connectToDB.php';
   include 'printResult.php';
+
   $success = True; //keep track of errors so it redirects the page only if there are no errors
   $db_conn = NULL; // edit the login credentials in connectToDB()
   $show_debug_alert_messages = False; // set to True if you want alerts to show you which methods are being triggered (see how it is used in debugAlertMessage())
+
+  // set timezone and get today's date
   date_default_timezone_set('America/Los_Angeles');
   $date = date("Y/m/d");
+
   function debugAlertMessage($message) {
     global $show_debug_alert_messages;
     if ($show_debug_alert_messages) {
       echo "<script type='text/javascript'>alert('" . $message . "');</script>";
     }
   }
+
   function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
-    //echo "<br>running ".$cmdstr."<br>";
-    global $db_conn, $success;
-    $statement = OCIParse($db_conn, $cmdstr);
-    //There are a set of comments at the end of the file that describe some of the OCI specific functions and how they work
-    if (!$statement) {
-      echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-      $e = OCI_Error($db_conn); // For OCIParse errors pass the connection handle
-      echo htmlentities($e['message']);
-      $success = False;
-    }
-    $r = OCIExecute($statement, OCI_DEFAULT);
-    if (!$r) {
-      echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-      $e = oci_error($statement); // For OCIExecute errors pass the statementhandle
-      echo htmlentities($e['message']);
-      $success = False;
-    }
-    return $statement;
-  }
-  function executeBoundSQL($cmdstr, $list) {
-    /* Sometimes the same statement will be executed several times with different values for the variables involved in the query.
-    In this case you don't need to create the statement several times. Bound variables cause a statement to only be
-    parsed once and you can reuse the statement. This is also very useful in protecting against SQL injection.
-    See the sample code below for how this function is used */
-    global $db_conn, $success;
-    $statement = OCIParse($db_conn, $cmdstr);
-    if (!$statement) {
-      echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-      $e = OCI_Error($db_conn);
-      echo htmlentities($e['message']);
-      $success = False;
-    }
-    foreach ($list as $tuple) {
-      foreach ($tuple as $bind => $val) {
-        //echo $val;
-        //echo "<br>".$bind."<br>";
-        OCIBindByName($statement, $bind, $val);
-        unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
+      //echo "<br>running ".$cmdstr."<br>";
+      global $db_conn, $success;
+
+      $statement = OCIParse($db_conn, $cmdstr);
+      //There are a set of comments at the end of the file that describe some of the OCI specific functions and how they work
+
+      if (!$statement) {
+          echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+          $e = OCI_Error($db_conn); // For OCIParse errors pass the connection handle
+          echo htmlentities($e['message']);
+          $success = False;
       }
+
       $r = OCIExecute($statement, OCI_DEFAULT);
       if (!$r) {
-        echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-        $e = OCI_Error($statement); // For OCIExecute errors, pass the statementhandle
-        echo htmlentities($e['message']);
-        echo "<br>";
-        $success = False;
+          echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+          $e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+          echo htmlentities($e['message']);
+          $success = False;
       }
-    }
+
+      return $statement;
   }
-  function printRentals() { //prints results from a select statement
+
+  // generates today's rentals report
+  function generateRentals() {
     global $date;
     $branchSelected = false;
     echo "<b><u>$date</u></b></b>" .strtoupper("<b><u> Daily Rentals Report</u></b><br>");
     if ($_GET['branch']=="all") {
-      echo strtoupper("<b><u>for all branches</u></b>");
+      echo strtoupper("<b>for all branches</b>");
       echo strtoupper("<br></br><br><b><u>Summary of results</u></b><br>");
       executePlainSQL("CREATE OR REPLACE VIEW rentalsToday AS SELECT * FROM rental where to_char(cast(rental.fromdt as date), 'YYYY/MM/DD')='$date'");
     } else {
@@ -141,7 +123,7 @@ OCILogon below to be your ORACLE username and password -->
     }
     printResult($result);
   }
-  function printReturns() { //prints results from a select statement
+  function generateReturns() { //prints results from a select statement
     global $date;
     $branchSelected = false;
     echo "<b><u>$date</u></b></b>" .strtoupper("<b><u> Daily Returns Report</u></b><br>");
@@ -188,94 +170,32 @@ OCILogon below to be your ORACLE username and password -->
       echo "<br>Retrieved vehicle data for branch<br>";
     } else {
       echo "<br>Retrieved vehicle data<br>";
-    }        printResult($result);
+    }
+    printResult($result);
   }
-  function disconnectFromDB() {
-    global $db_conn;
-    debugAlertMessage("Disconnect from Database");
-    OCILogoff($db_conn);
-  }
-  function handleUpdateRequest() {
-    global $db_conn;
-    $old_name = $_POST['oldName'];
-    $new_name = $_POST['newName'];
-    // you need the wrap the old name and new name values with single quotations
-    executePlainSQL("UPDATE demoTable SET name='" . $new_name . "' WHERE name='" . $old_name . "'");
-    OCICommit($db_conn);
-  }
-  function handleResetRequest() {
-    global $db_conn;
-    // Drop old table
-    executePlainSQL("DROP TABLE demoTable");
-    // Create new table
-    echo "<br> creating new table <br>";
-    executePlainSQL("CREATE TABLE demoTable (id int PRIMARY KEY, name char(30))");
-    OCICommit($db_conn);
-  }
-  function handleInsertRequest() {
-    global $db_conn;
-    //Getting the values from user and insert data into the table
-    $tuple = array (
-      ":bind1" => $_POST['insNo'],
-      ":bind2" => $_POST['insName']
-    );
-    $alltuples = array (
-      $tuple
-    );
-    executeBoundSQL("insert into demoTable values (:bind1, :bind2)", $alltuples);
-    OCICommit($db_conn);
-  }
+
   function handleGenerateRequest() {
     global $db_conn;
-    global $rentals_status;
-    global $returns_status;
     echo "<br></br>";
     if (isset($_GET['reportType']) && $_GET['reportType']=="rentals") {
-      $rentals_status = 'checked';
-      printRentals();
+      generateRentals();
     } else if (isset($_GET['reportType']) && $_GET['reportType']=="returns") {
-      $returns_status = 'checked';
-      printReturns();
-    }
-  }
-  function handleCountRequest() {
-    global $db_conn;
-    $rentals = executePlainSQL("SELECT Count(*) FROM rental");
-    if (($row = oci_fetch_row($rentals)) != false) {
-      echo "<br> The number of tuples in rental: " . $row[0] . "<br>";
-    }
-    $returns = executePlainSQL("SELECT Count(*) FROM return");
-    if (($row = oci_fetch_row($returns)) != false) {
-      echo "<br> The number of tuples in return: " . $row[0] . "<br>";
-    }
-  }
-  // HANDLE ALL POST ROUTES
-  // A better coding practice is to have one method that reroutes your requests accordingly. It will make it easier to add/remove functionality.
-  function handlePOSTRequest() {
-    if (connectToDB()) {
-      if (array_key_exists('resetTablesRequest', $_POST)) {
-        handleResetRequest();
-      } else if (array_key_exists('updateQueryRequest', $_POST)) {
-        handleUpdateRequest();
-      } else if (array_key_exists('insertQueryRequest', $_POST)) {
-        handleInsertRequest();
-      }
-      disconnectFromDB();
+      generateReturns();
     }
   }
   // HANDLE ALL GET ROUTES
   // A better coding practice is to have one method that reroutes your requests accordingly. It will make it easier to add/remove functionality.
   function handleGETRequest() {
     if (connectToDB()) {
-      if (array_key_exists('countTuples', $_GET)) {
-        handleCountRequest();
-      } else if (array_key_exists('generateReport', $_GET)) {
+      if (array_key_exists('generateReport', $_GET)) {
         handleGenerateRequest();
       }
       disconnectFromDB();
     }
   }
+
   ?>
+
   <h2>Generate reports</h2>
   <hr />
   <form method="GET" action="clerk_generate_report.php"> <!--refresh page when submitted-->
@@ -300,11 +220,10 @@ OCILogon below to be your ORACLE username and password -->
   </form>
 
   <?php
-  if (isset($_POST['reset']) || isset($_POST['updateSubmit']) || isset($_POST['insertSubmit'])) {
-    handlePOSTRequest();
-  } else if (isset($_GET['countTupleRequest']) || isset($_GET['generateReportRequest'])) {
+  if (isset($_GET['generateReportRequest'])) {
     handleGETRequest();
   }
   ?>
+
 </body>
 </html>
